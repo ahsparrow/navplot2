e636369dccd5b6e62aac6acf20bc0dfe983912a6
Alan Sparrow <git@freeflight.org.uk>
2022-01-24 17:55:38 +0000

    Change to Google drive

--- heroku.py
+++ heroku.py
@@ -17,54 +17,73 @@
 # along with YAIXM.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
+import json
 import io
 import os
 
-import dropbox
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from googleapiclient.http import MediaIoBaseUpload
 
 from navplot import get_notams, make_briefing
 
 # Map origin and scaling
-SOUTH = (50.2, -5.0, 6.5)
-NORTH = (53.0, -6.0, 6.0)
+SOUTH_EXTENTS = (50.2, -5.0, 6.5)
+NORTH_EXTENTS = (53.0, -6.0, 6.0)
 
 HOURS = list(range(6, 18))
 
-def upload_dropbox(name, now, data):
-    app_key = os.environ['DROPBOX_APP_KEY']
-    app_secret = os.environ['DROPBOX_APP_SECRET']
-    refresh_token = os.environ['DROPBOX_REFRESH_TOKEN']
+TODAY_SOUTH_ID = "1CChND79djTLhRd5yy3U0UOkyV_VGZmUH"
+TODAY_NORTH_ID = "1Zipk8AjVATio21s_yk7OlZaFhuFQd1DK"
+TOMORROW_SOUTH_ID = "1sv5SiHiSgFFCDboZnIjHn21WFhvzcdll"
+TOMORROW_NORTH_ID = "1-RRiOlcY7z80tvsFQ3Sm1iE5T3-_Wpw-"
 
-    dbx = dropbox.Dropbox(
-            app_key=app_key,
-            app_secret=app_secret,
-            oauth2_refresh_token=refresh_token)
+# Get Google drive API service
+def get_service(api_name, api_version, scopes, account_info):
+    credentials = service_account.Credentials.from_service_account_info(
+            account_info)
+    scoped_credentials = credentials.with_scopes(scopes)
 
-    dbx.files_upload(
-            data, "/" + name + ".pdf", dropbox.files.WriteMode.overwrite)
+    service = build(api_name, api_version, credentials=scoped_credentials)
+    return service
+
+# Upload data to file on Google drive
+def upload_google(service, file_id, data):
+    media = MediaIoBaseUpload(data, mimetype="application/pdf")
+    service.files().update(fileId=file_id, media_body=media).execute()
 
 now = datetime.datetime.utcnow().replace(microsecond=0)
 
 if now.hour in HOURS:
+    # Get NOTAM data
     notam_soup = get_notams()
-    date = now.date()
+
+    # Authenticate and construct service.
+    account_info = json.loads(os.environ['SERVICE_ACCOUNT_KEY'])
+    service = get_service(
+        api_name='drive',
+        api_version='v3',
+        scopes=['https://www.googleapis.com/auth/drive'],
+        account_info=account_info)
 
     # Today's NOTAMs
-    buf = io.BytesIO()
-    make_briefing(buf, notam_soup, date, SOUTH)
-    upload_dropbox("today_south", now, buf.getvalue())
+    date = now.date()
 
     buf = io.BytesIO()
-    make_briefing(buf, notam_soup, date, NORTH)
-    upload_dropbox("today_north", now, buf.getvalue())
+    make_briefing(buf, notam_soup, date, SOUTH_EXTENTS)
+    upload_google(service, TODAY_SOUTH_ID, buf)
+
+    buf = io.BytesIO()
+    make_briefing(buf, notam_soup, date, NORTH_EXTENTS)
+    upload_google(service, TODAY_NORTH_ID, buf)
 
     # Tomorrow's NOTAMs
     date += datetime.timedelta(days=1)
 
     buf = io.BytesIO()
-    make_briefing(buf, notam_soup, date, SOUTH)
-    upload_dropbox("tomorrow_south", now, buf.getvalue())
+    make_briefing(buf, notam_soup, date, SOUTH_EXTENTS)
+    upload_google(service, TOMORROW_SOUTH_ID, buf)
 
     buf = io.BytesIO()
-    make_briefing(buf, notam_soup, date, NORTH)
-    upload_dropbox("tomorrow_north", now, buf.getvalue())
+    make_briefing(buf, notam_soup, date, NORTH_EXTENTS)
+    upload_google(service, TOMORROW_NORTH_ID, buf)
