edf307bf062eb1d78ab9eaa8342dd0671591dc35
Alan Sparrow <git@freeflight.org.uk>
2022-09-02 16:16:09 +0100

    Remove systemd service file

--- today.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-$1/bin/python navplot.py $2/today_south.pdf
-$1/bin/python navplot.py --north $2/today_north.pdf
--- tomorrow.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-$1/bin/python navplot.py --tomorrow $2/tomorrow_south.pdf
-$1/bin/python navplot.py --tomorrow --north $2/tomorrow_north.pdf
